6af21c3c02a17ba1420fbbb1c0c95991f0d0e8e1
zengjiayi <1668026348qq.com>
2021-03-29 13:44:59 +0800

    4.HW-CE1 Commit

---
+++ src/main/java/com/zengjiayi/week4/demo/ConfigDemoServlet.java
@@ -0,0 +1,35 @@
+package com.zengjiayi.week4.demo;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+//use @WebServlet - no web.xml code
+@WebServlet(
+        urlPatterns = {"/config"},
+        initParams ={
+                @WebInitParam(name = "name", value = "Zeng Jiayi"),
+                @WebInitParam(name = "studentId", value = "2019211001001202")
+        }
+)
+public class ConfigDemoServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = getServletConfig().getInitParameter("name");
+        String studentId = getServletConfig().getInitParameter("studentId");
+        PrintWriter writer = response.getWriter();
+        writer.println("<br/>name:" + name);
+        writer.println("<br/>studentId:" + studentId);
+    }
+}
