1a83fb22490cb2302b86f612ed88b78eabbddf97
zengjiayi <1668026348qq.com>
2021-04-11 21:49:35 +0800

    Hw-week 6 commit

---
+++ src/main/java/com/zengjiayi/week6/demo/RedirectServlet.java
@@ -0,0 +1,21 @@
+package com.zengjiayi.week6.demo;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "RedirectServlet", value = "/redirect")
+public class RedirectServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("before redirect ");
+        System.out.println("after redirect ");
+        response.sendRedirect("https://www.baidu.com");
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+}
+
